0cfdf33184c509f750d75c7111d9c5d0b1129c32
JoGyeonggu <wh941104@gmail.com>
2024-10-24 17:26:43 +0900

    no message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index.html');
+router.get("/", function (req, res, next) {
+  res.render("dashboard.html");
+});
+
+/* GET game creation page. */
+router.get("/game", function (req, res, next) {
+  res.render("game.html");
+});
+
+/* GET dashboard page. */
+router.get("/player", function (req, res, next) {
+  res.render("player.html");
+});
+
+/* GET dashboard page. */
+router.get("/dashboard", function (req, res, next) {
+  res.render("dashboard.html");
 });
 
 module.exports = router;
